9d17fde7814871cb32b29c37c93a1680a49befda
kasperzvn <kasperhavemus@gmail.com>
2024-07-24 10:56:11 +0200

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        
+        //MAIN
+        payton: ["Paytone One", ...defaultTheme.fontFamily.sans],
+        yatra: ["Yatra One", ...defaultTheme.fontFamily.sans],
+        gochi: ["Gochi Hand", ...defaultTheme.fontFamily.sans],
+        jaro: ["Jaro", ...defaultTheme.fontFamily.sans],
+
+
+      }
+    },
   },
   plugins: [],
 }
